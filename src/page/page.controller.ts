0b37eff49ab33544107fa58c094767734f4da13d
blueeon <i@blueeon.me>
2024-07-19 22:37:37 +0800

    ✨ feat: add demo page

--- src/page/page.controller.ts
+++ src/page/page.controller.ts
@@ -1,10 +1,21 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Get, Controller, Render, Query } from '@nestjs/common';
 
-@Controller('/demo')
+@Controller('/')
 export class PageController {
-  @Get('/index')
+  @Get('/')
   @Render('index')
-  root() {
-    return { message: 'Hello world!' };
+  root(@Query('query') query?: string) {
+    console.log("query:", query);
+    if (query) {
+      const message = `The images depict various cats, including a gray cat sitting on a shelf, a cute and adorable kitten, a close-up portrait of a black and white cat: ![https://picsum.photos/600](https://picsum.photos/600)\n![https://picsum.photos/600](https://picsum.photos/600)`;
+      console.log("Answer:", message);
+      return {
+        message: message,
+        query: query || null
+      };
+    }
+    return {
+      query: query || null
+    };
   }
 }
